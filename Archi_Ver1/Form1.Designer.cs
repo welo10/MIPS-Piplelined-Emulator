da2a7c27de492dd20bc442d2b1c86e77ec13069d
Waleed Ehab Badr <waleedehab99@gmail.com>
2020-08-27 17:22:06 +0200

    Added convert feature

--- Archi_Ver1/Form1.Designer.cs
+++ Archi_Ver1/Form1.Designer.cs
@@ -31,20 +31,24 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.x = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Registers = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.Registers = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.x = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
@@ -55,17 +59,17 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.Size = new System.Drawing.Size(80, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "User Code";
+            this.label1.Text = "Assembly Code";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(15, 36);
+            this.textBox1.Location = new System.Drawing.Point(12, 300);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(256, 323);
+            this.textBox1.Size = new System.Drawing.Size(266, 165);
             this.textBox1.TabIndex = 1;
             // 
             // dataGridView1
@@ -78,6 +82,16 @@
             this.dataGridView1.Size = new System.Drawing.Size(504, 323);
             this.dataGridView1.TabIndex = 2;
             // 
+            // x
+            // 
+            this.x.HeaderText = "Registers";
+            this.x.Name = "x";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Value";
+            this.Column1.Name = "Column1";
+            // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -89,9 +103,19 @@
             this.dataGridView2.Size = new System.Drawing.Size(504, 277);
             this.dataGridView2.TabIndex = 3;
             // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Registers";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Value";
+            this.Column3.Name = "Column3";
+            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(75, 389);
+            this.button2.Location = new System.Drawing.Point(86, 512);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(112, 56);
             this.button2.TabIndex = 5;
@@ -101,7 +125,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(75, 483);
+            this.button3.Location = new System.Drawing.Point(86, 574);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(112, 56);
             this.button3.TabIndex = 6;
@@ -113,7 +137,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(52, 559);
+            this.label2.Location = new System.Drawing.Point(63, 650);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(168, 25);
             this.label2.TabIndex = 7;
@@ -129,6 +153,16 @@
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.Size = new System.Drawing.Size(262, 648);
             this.dataGridView3.TabIndex = 8;
+            // 
+            // Registers
+            // 
+            this.Registers.HeaderText = "Column1";
+            this.Registers.Name = "Registers";
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Column1";
+            this.Value.Name = "Value";
             // 
             // label3
             // 
@@ -161,41 +195,52 @@
             this.label5.TabIndex = 11;
             this.label5.Text = "MIPS Registers";
             // 
-            // Registers
+            // textBox2
             // 
-            this.Registers.HeaderText = "Column1";
-            this.Registers.Name = "Registers";
+            this.textBox2.Location = new System.Drawing.Point(15, 36);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(266, 165);
+            this.textBox2.TabIndex = 13;
             // 
-            // Value
+            // label6
             // 
-            this.Value.HeaderText = "Column1";
-            this.Value.Name = "Value";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Machine Code";
             // 
-            // x
+            // button1
             // 
-            this.x.HeaderText = "Registers";
-            this.x.Name = "x";
+            this.button1.Location = new System.Drawing.Point(26, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 36);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Convert To Machine Code";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Column1
+            // button4
             // 
-            this.Column1.HeaderText = "Value";
-            this.Column1.Name = "Column1";
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "Registers";
-            this.Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Value";
-            this.Column3.Name = "Column3";
+            this.button4.Location = new System.Drawing.Point(172, 227);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 36);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Convert To Assembly Code";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1370, 720);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -237,6 +282,10 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Archi_Ver1/Form1.cs
+++ Archi_Ver1/Form1.cs
@@ -34,21 +34,10 @@ namespace MIPS_Emulator
             c = 0;
             label2.Text = "Cycle Number: " + c;
         }
-        private void button2_Click(object sender, EventArgs e)
+        private void init()
         {
-            clr();
-            mips = new MIPS();
-            mips.init();
-            for(int i = 0; i < 32; i++)
-            {
-                dataGridView3.Rows.Add(new string[] {"$"+i, mips.registers[i].ToString() });
-            }
-            for (int i = 0; i < 1024; i++)
-            {
-                dataGridView2.Rows.Add(new string[] { "$" + i, "99" });
-            }
             string[] r = textBox1.Text.Split('\n');
-            for(int i = 0; i < r.Length; i++)
+            for (int i = 0; i < r.Length; i++)
             {
                 if (r[i].Length >= 4)
                 {
@@ -57,12 +46,35 @@ namespace MIPS_Emulator
                     for (int j = 0; j < tmp[1].Length; j++)
                         if (tmp[1][j] != ' ')
                             s += tmp[1][j];
-                    mips.Instructions.Add(Convert.ToInt32(tmp[0]),s);
+                    mips.Instructions.Add(Convert.ToInt32(tmp[0]), s);
                     mips.FetchQ.Enqueue(tmp[0]);
                 }
             }
             if (mips.FetchQ.Count > 0)
                 mips.StagesQ[0] = mips.FetchQ.Dequeue().ToString();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clr();
+            mips = new MIPS();
+            mips.init();
+            for (int i = 0; i < 32; i++)
+            {
+                dataGridView3.Rows.Add(new string[] { "$" + i, mips.registers[i].ToString() });
+            }
+            for (int i = 0; i < 1024; i++)
+            {
+                dataGridView2.Rows.Add(new string[] { "$" + i, "99" });
+            }
+            if (textBox1.Text.Length < 4)
+            {
+                convert_from_assembly_to_machine();
+            }
+            if (textBox2.Text.Length < 4)
+            {
+                convert_from_machine_to_assembly();
+            }
+            init();
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -72,7 +84,7 @@ namespace MIPS_Emulator
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
             c = 0;
             clr();
         }
@@ -82,40 +94,194 @@ namespace MIPS_Emulator
             c++;
             label2.Text = "Cycle Number: " + c;
             int[] found = new int[] { 0, 0, 0, 0, 0 };
-            for(int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
-                if (mips.StagesQ[i]!="")
+                if (mips.StagesQ[i] != "")
                 {
                     found[i] = 1;
                 }
             }
             mips.Cycle();
-       //     MessageBox.Show(mips.IF_ID[0] + mips.IF_ID[1]);
-             string[] IF_ID = new string []{"Address","Instruction"};
-         string[] ID_EX = new string[] {"EX","M","WB","Address","Read Data1","ReadData2","Sign Extend","Instruction[20-16]","Instruction[15-11]"};
-         string[] Ex_MEM = new string[] {"M","WB","Address","Zero","Alu Result","Read Data 2","Mux"};
-         string[] MEM_WB = new string[] {"WB","Read Data","ALU Result","Mux" };
-            if(found[0]>0)
-            for (int i = 0; i < 2; i++)
-            {
-                dataGridView1.Rows.Add(new string[] { "IF/ID:" + IF_ID[i], mips.IF_ID[i].ToString() });
-            }
+            //     MessageBox.Show(mips.IF_ID[0] + mips.IF_ID[1]);
+            string[] IF_ID = new string[] { "Address", "Instruction" };
+            string[] ID_EX = new string[] { "EX", "M", "WB", "Address", "Read Data1", "ReadData2", "Sign Extend", "Instruction[20-16]", "Instruction[15-11]" };
+            string[] Ex_MEM = new string[] { "M", "WB", "Address", "Zero", "Alu Result", "Read Data 2", "Mux" };
+            string[] MEM_WB = new string[] { "WB", "Read Data", "ALU Result", "Mux" };
+            if (found[0] > 0)
+                for (int i = 0; i < 2; i++)
+                {
+                    dataGridView1.Rows.Add(new string[] { "IF/ID:" + IF_ID[i], mips.IF_ID[i].ToString() });
+                }
             if (found[1] > 0)
                 for (int i = 0; i < 9; i++)
+                {
+                    dataGridView1.Rows.Add(new string[] { "ID/EX:" + ID_EX[i], mips.ID_EX[i].ToString() });
+
+                }
+            if (found[2] > 0)
+                for (int i = 0; i < 7; i++)
+                {
+                    dataGridView1.Rows.Add(new string[] { "EX/MEM:" + Ex_MEM[i], mips.Ex_MEM[i].ToString() });
+                }
+            if (found[3] > 0)
+                for (int i = 0; i < 4; i++)
+                {
+                    dataGridView1.Rows.Add(new string[] { "MEM/WB:" + MEM_WB[i], mips.MEM_WB[i].ToString() });
+                }
+        }
+        private string formatBin(string Bin, int bits)
+        {
+            string ret = "";
+            for (int i = 0; i < bits - Bin.Length; i++)
+                ret += "0";
+            ret += Bin;
+            return ret;
+        }
+        private string Assembly_To_machine(char type, string funct, string rs, string rt, string rd)
+        {
+            string ret = "";
+            if (type == 'R')
             {
-                dataGridView1.Rows.Add(new string[] { "ID/EX:" + ID_EX[i], mips.ID_EX[i].ToString() });
+                ret = "000000";
+                string tmp =
+                ret += formatBin(Convert.ToString(Convert.ToInt32(rs.Substring(1)), 2), 5);
+                ret += formatBin(Convert.ToString(Convert.ToInt32(rt.Substring(1)), 2), 5);
+                ret += formatBin(Convert.ToString(Convert.ToInt32(rd.Substring(1)), 2), 5);
+                ret += "00000";
+
+                if (funct == "add") ret += "100000";
+                else if (funct == "sub") ret += "100010";
+                else if (funct == "and") ret += "100100";
+                else if (funct == "or") ret += "100101";
+
 
             }
-            if(found[2]>0)
-            for (int i = 0; i <7; i++)
+            else
             {
-                dataGridView1.Rows.Add(new string[] { "EX/MEM:" + Ex_MEM[i], mips.Ex_MEM[i].ToString() });
+                ret = "100011";
+
+
+                ret += formatBin(Convert.ToString(Convert.ToInt32(rs.Substring(0)), 2), 5);
+                ret += formatBin(Convert.ToString(Convert.ToInt32(rt.Substring(1)), 2), 5);
+                ret += formatBin(Convert.ToString(Convert.ToInt32(rd), 2), 16);
             }
-            if (found[3] > 0)
-            for (int i = 0; i < 4; i++)
+            return ret;
+        }
+        private void convert_from_assembly_to_machine()
+        {
+            string machine = "";
+            if (textBox2.Text.Length > 4)
             {
-                dataGridView1.Rows.Add(new string[] { "MEM/WB:" + MEM_WB[i], mips.MEM_WB[i].ToString() });
+                string[] r = textBox2.Text.Split('\n');
+                for (int i = 0; i < r.Length; i++)
+                {
+                    if (r[i].Length >= 4)
+                    {
+                        string[] tmp = r[i].Split(':');
+                        machine += tmp[0] + ": ";
+                        string code = "";
+                        if (tmp[1].Substring(1, 2) == "lw")
+                        {
+                            string rs, rt, rd, funct;
+                            string[] tmp_regs;
+                            tmp_regs = tmp[1].Substring(3).Split(',');
+                            funct = tmp[1].Substring(1, 2);
+                            rt = tmp_regs[0].Substring(1);
+                            rd = tmp_regs[1].Substring(1, 1);
+                            rs = tmp_regs[1].Substring(3);
+                            rs = rs.Substring(0, rs.Length - 3);
+                            code = Assembly_To_machine('I', funct, rs, rt, rd);
+
+                        }
+                        else
+                        {
+                            string[] tmp_regs;
+                            string rs, rt, rd, funct;
+
+                            if (tmp[1].Substring(1, 2) == "or")
+                            {
+                                tmp_regs = tmp[1].Substring(3).Split(',');
+                                funct = "or";
+                            }
+                            else
+                            {
+                                tmp_regs = tmp[1].Substring(4).Split(',');
+                                funct = tmp[1].Substring(1, 3);
+
+                            }
+                            rd = tmp_regs[0].Substring(1);
+                            rs = tmp_regs[1].Substring(1);
+                            rt = tmp_regs[2].Substring(1);
+                            code = Assembly_To_machine('R', funct, rs, rt, rd);
+                        }
+                        machine += code + System.Environment.NewLine;
+
+                    }
+                }
+                textBox1.Text = machine;
             }
+            /*
+1000: and $3, $4, $30
+1004: add $6, $8, $20
+1008: lw $6, 8(9$)
+1012: sub $10, $9, $5
+1016: or $3, $16, $8
+             */
+
+        }
+        private string convert_from_machine_to_assembly()
+        {
+            string assembly = "";
+            string[] r = textBox1.Text.Split('\n');
+            for (int i = 0; i < r.Length; i++)
+            {
+                if (r[i].Length >= 4)
+                {
+                    string[] tmp = r[i].Split(':');
+                    string row = tmp[0] + ": ";
+                    string s = "";
+                    for (int j = 0; j < tmp[1].Length; j++)
+                        if (tmp[1][j] != ' ')
+                            s += tmp[1][j];
+                    string funct, rs, rt, rd;
+                    if (s.Substring(0, 6) == "000000") //R-Type
+                    {
+
+                        rs = s.Substring(6, 5);
+                        rt = s.Substring(11, 5);
+                        rd = s.Substring(16, 5);
+                        funct = s.Substring(26, 6);
+                        string fun = "";
+                        if (funct == "100000") fun = "add";
+                        else if (funct == "100010") fun = "sub";
+                        else if (funct == "100100") fun = "and";
+                        else if (funct == "100101") fun = "or";
+                        row += fun + " $" + Convert.ToInt32(rd, 2) + ", $" + Convert.ToInt32(rs, 2) + ", $" + Convert.ToInt32(rt, 2) + "\n";
+                    }
+                    else //I-Type
+                    {
+                        rs = s.Substring(6, 5);
+                        rt = s.Substring(11, 5);
+                        rd = s.Substring(16, 16);
+                        funct = s.Substring(0, 6);
+                        string fun = "";
+                        if (funct == "100011") fun = "lw";
+                        row += fun + " $" + Convert.ToInt32(rt, 2) + ", " + Convert.ToInt32(rd, 2) + "(" + Convert.ToInt32(rs, 2) + "$)";
+                    }
+                    assembly += row + System.Environment.NewLine;
+                }
+                textBox2.Text = assembly;
+            }
+            return assembly;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            convert_from_assembly_to_machine();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            convert_from_machine_to_assembly();
         }
     }
 }
